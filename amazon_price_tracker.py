f26d7e4615c850396c2cc5b338f40e34be34845a
AsaiAshita <114286827+AsaiAshita@users.noreply.github.com>
2026-08-13 11:29:29 +0200

    y axis in terminal now shows real prices

--- amazon_price_tracker.py
+++ amazon_price_tracker.py
@@ -536,6 +536,7 @@ def main():
                         plt.ticks_color(ticks_color)
 
                     plt.xticks(x, labels)
+                    plt.yticks(y,y)
                     plt.title("Price History for: " + (result_list[menu_entry_index].lstrip() if len(result_list[menu_entry_index].lstrip())<50 else result_list[menu_entry_index].lstrip()[:50]+ "..."))
                     start = max(0, len(x) - 20)
                     end = len(x)
